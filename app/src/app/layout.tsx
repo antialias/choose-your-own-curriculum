a5558071ddddea194bd5a5792179298944720b34
Thomas Hallock <antialias@users.noreply.github.com>
2025-07-07 09:55:44 -0500

    prefetch session for navbar

--- app/src/app/layout.tsx
+++ app/src/app/layout.tsx
@@ -3,21 +3,24 @@ import "./globals.css";
 import "../styles/index.css";
 import AuthProvider from "@/components/AuthProvider";
 import { NavBar } from "@/components/NavBar";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/authOptions";
 
 export const metadata: Metadata = {
   title: "Choose Your Own Curriculum",
   description: "Track and plan your learning journey",
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const session = await getServerSession(authOptions);
   return (
     <html lang="en">
       <body>
-        <AuthProvider>
+        <AuthProvider session={session}>
           <NavBar />
           {children}
         </AuthProvider>
