98cc378791615e2f0177ab8c04dc648f75a82504
Thomas Hallock <thomas@evolutioniq.com>
2025-07-07 13:49:12 -0500

    using sqlite-vec

--- app/src/db/index.ts
+++ app/src/db/index.ts
@@ -2,8 +2,10 @@ import fs from 'fs';
 import Database from 'better-sqlite3';
 import { drizzle } from 'drizzle-orm/better-sqlite3';
 import { migrate } from 'drizzle-orm/better-sqlite3/migrator';
+import { load as loadVec } from 'sqlite-vec';
 
 const sqlite = new Database(process.env.DATABASE_URL || './sqlite.db');
+loadVec(sqlite);
 export const db = drizzle(sqlite);
 
 if (fs.existsSync('./drizzle') && process.env.NODE_ENV !== 'production') {
