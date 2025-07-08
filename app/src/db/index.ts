69d3bfc2c513b3cfd610662c85ac5a02d6125182
Thomas Hallock <antialias@users.noreply.github.com>
2025-07-08 17:18:09 -0500

    refactor(db): use builtin vss

--- app/src/db/index.ts
+++ app/src/db/index.ts
@@ -3,14 +3,12 @@ import Database from 'better-sqlite3';
 import type { Database as SqliteDatabase } from 'better-sqlite3';
 import { drizzle, type BetterSQLite3Database } from 'drizzle-orm/better-sqlite3';
 import { migrate } from 'drizzle-orm/better-sqlite3/migrator';
-import { load as loadVec } from 'sqlite-vec';
 
 let sqlite: SqliteDatabase | null = null;
 
 export function getSqlite() {
   if (!sqlite) {
     sqlite = new Database(process.env.DATABASE_URL || './sqlite.db');
-    loadVec(sqlite);
   }
   return sqlite;
 }
