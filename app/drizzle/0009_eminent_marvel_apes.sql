ecd559a9ab69211d62019a56bd87ba27af57cc5e
Thomas Hallock <thomas@evolutioniq.com>
2025-07-08 21:57:05 -0500

    removed evil statment end thing

--- app/drizzle/0009_eminent_marvel_apes.sql
+++ app/drizzle/0009_eminent_marvel_apes.sql
@@ -2,4 +2,4 @@ ALTER TABLE `uploaded_work` ADD `grade` text;--> statement-breakpoint
 ALTER TABLE `uploaded_work` ADD `extractedStudentName` text;--> statement-breakpoint
 ALTER TABLE `uploaded_work` ADD `extractedDateOfWork` text;--> statement-breakpoint
 ALTER TABLE `uploaded_work` ADD `masteryPercent` real;--> statement-breakpoint
-ALTER TABLE `uploaded_work` ADD `feedback` text;--> statement-breakpoint
+ALTER TABLE `uploaded_work` ADD `feedback` text;
